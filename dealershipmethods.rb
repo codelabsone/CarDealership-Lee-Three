617a73c0f16b000f7968a4b9ee31f787483defe7
jjohnkoski <jacobjohnkoski@gmail.com>
2019-08-01 20:13:37 -0500

    8-1-19 revision

--- dealershipmethods.rb
+++ dealershipmethods.rb
@@ -1,5 +1,46 @@
 module DealershipMethods
 
-    
+    def self.greeting
+        puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$".center(100) + "\n\n"
+        puts "E  U  R  O  P  E  A  N".center(100) + "\n\n"
+        puts "I  M  P  O  R  T  S".center(100) + "\n\n"
+        puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$".center(100) + "\n\n"
+        puts "Welcome to European Imports: Only the finest imported sports cars. How may we help you today?".center(100)
+        puts "\n\t1. List All Vehicles\n\t2. List Vehicles in Budget\n\t3. Vehicle Search\n\t4. Make Purchase\n\t5. Sell Vehicle\n\t6. Exit\n"
+
+    end
+
+    def self.search(inventory)
+        puts "\nSearch by make, model, color, or year:\n"
+        criteria = gets.chomp
+        if criteria.to_i.to_s.eql?(criteria)
+            criteria = criteria.to_i
+        else
+            criteria = criteria.capitalize
+        end
+        puts "\nDisplaying Matches:\n"
+        inventory.each do |car|
+            if criteria === car.make
+                puts car
+            elsif criteria === car.model
+                puts car
+            elsif criteria === car.color
+                puts car
+            elsif criteria === car.year
+                puts car
+            else
+            end
+        end
+
+    end
+
+    def self.budget_listings(inventory)
+        puts "\nPlease enter your budget-$$:"
+        budget = gets.chomp.to_i
+        inventory.each do |car|
+            puts car if car.price <= budget
+        end
+
+    end
 
 end
--- inventory.rb
+++ inventory.rb
@@ -3,8 +3,7 @@ require_relative 'dealershipmethods'
 
 class Inventory
 
-    attr_accessor :listings, :budget, :sale
-    attr_reader :year, :make, :model, :color, :price
+    attr_accessor :listings, :budget, :sale, :year, :make, :model, :color, :price
     def initialize
         @listings = []
         @profits = 0
@@ -15,11 +14,33 @@ class Inventory
     end
 
     def display_listings
+        puts "\nThis is our entire inventory:\n".center(100)
         puts @listings
     end
 
     def purchase
-        
+        #needs written
+    end
+
+    def menu
+        DealershipMethods.greeting
+        choice = gets.chomp.to_i
+        case choice
+            when 1
+                self.display_listings
+                self.menu_return
+            when 2
+                DealershipMethods.budget_listings(@listings)
+                self.menu_return
+            when 3
+                DealershipMethods.search(@listings)
+                self.menu_return
+            when 4
+                self.purchase
+                self.menu_return
+            when 5
+                #exit
+        end
     end
 
     def menu_return
@@ -27,79 +48,11 @@ class Inventory
         puts "\n\t1. Purchase Vehicle\n\t2. Return to Menu\n\t3. Exit\n"
         choice = gets.chomp.to_i
         case choice
-        when 1
-            self.purchase
-        when 2
-            self.menu
-        when 3
-        end
-    end
-
-
-
-    def menu
-        puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$".center(100) + "\n\n"
-        puts "E  U  R  O  P  E  A  N".center(100) + "\n\n"
-        puts "I  M  P  O  R  T  S".center(100) + "\n\n"
-        puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$".center(100) + "\n\n"
-        puts "Welcome to European Imports: Only the finest imported sports cars. How may we help you today?".center(100)
-        puts "\n\t1. List All Vehicles\n\t2. List Vehicles in Budget\n\t3. Vehicle Search\n\t4. Make Purchase\n\t5. Sell Vehicle\n\t6. Exit\n"
-
-        choice = gets.chomp.to_i
-        case choice
-        when 1
-            puts "\nThis is our entire inventory:\n".center(100)
-            puts @listings
-            self.menu_return
-
-        when 2
-            puts "\nPlease enter your budget...$$:"
-            budget = gets.chomp.to_i
-            puts "\nThese are the vehicles in your budget:\n"
-            @listings.each do |car|
-                puts car if car.price <= budget
-            end
-            self.menu_return
-        when 3
-            puts "\nSearch by:"
-            puts "\n\t1. Year\n\t2. Make\n\t3. Model\n\t4. Color\n"
-            search_choice = gets.chomp.to_i
-            puts "\n"
-            case search_choice
             when 1
-                puts "\nWhat year are you looking for ?\n"
-                year_choice = gets.chomp.to_i
-                puts "\n"
-                @listings.each do |car|
-                    puts car if car.year == year_choice
-                end
+                #self.purchase
             when 2
-                puts "\nWhat make are you looking for ?\n"
-                make_choice = gets.chomp.capitalize
-                puts "\n"
-                @listings.each do |car|
-                    puts car if car.make == make_choice
-                end
+                self.menu
             when 3
-                puts "\nWhat model are you looking for ?\n"
-                model_choice = gets.chomp.capitalize
-                puts "\n"
-                @listings.each do |car|
-                    puts car if car.model == model_choice
-                end
-            when 4
-                puts "\nWhat color are you looking for ?\n"
-                color_choice = gets.chomp.capitalize
-                puts "\n"
-                @listings.each do |car|
-                    puts car if car.color == color_choice
-                end
-            end
-            self.menu_return
-        when 4
-            self.purchase
-        when 5
-
         end
     end
 
