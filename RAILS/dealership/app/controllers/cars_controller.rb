2e36515a1c70df8531d88ed18a531b974e216f50
jjohnkoski <jacobjohnkoski@gmail.com>
2019-09-12 19:00:28 -0500

    Overhaul, more user friendly.

--- RAILS/dealership/app/controllers/cars_controller.rb
+++ RAILS/dealership/app/controllers/cars_controller.rb
@@ -14,10 +14,31 @@ class CarsController < ApplicationController
 
     def update
         @car = Car.find(params[:id])
-        car_params = params.require(:car).
-        permit(:make, :model, :year, :color, :price)
         @car.update(car_params)
         redirect_to @car
+    end
+
+    def new
+        @car = Car.new
+    end
+
+    def create
+        @car = Car.new(car_params)
+        @car.save
+        redirect_to @car
+    end
+
+    def destroy
+        @car = Car.find(params[:id])
+        @car.destroy
+        redirect_to root_path
+    end
+
+private
+
+    def car_params
+        params.require(:car).
+        permit(:make, :model, :color, :year, :price)
     end
 
 end
--- RAILS/dealership/config/routes.rb
+++ RAILS/dealership/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
     root "cars#index"
-    get "cars" => "cars#index"
-    get "cars/:id" => "cars#show", as: "car"
-    get "cars/:id/edit" => "cars#edit", as: "edit_car"
-    patch "cars/:id" => "cars#update"
+    resources :cars
 end
